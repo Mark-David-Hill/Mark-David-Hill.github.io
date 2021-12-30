d380f210c4323d7106dbe5e402299abb055f84d6
Akamarak <67317533+Mark-David-Hill@users.noreply.github.com>
2021-12-30 16:28:01 +0900

    Make adjustments to MM JSON file, set up test files for displaying the information about a single game.

--- mm/displayGameTest.js
+++ mm/displayGameTest.js
@@ -17,38 +17,7 @@ xhr.onload = function() {
 } // end onload
 
 
-
-// Display all games
-function displayGames() {
-    content = "";
-
-    // loop through each game
-    for (let i in mmData.games) {
-         //i is is the key for each game
-        let game = mmData.games[i];
-        // Generate HTML content for each game card
-        content += 
-            `<div class="col">
-                <div class="card shadow-sm"> 
-                    <img src="images/${game.id}.png" class="img-fluid" alt="${game} box-art"> <div class="card-body">
-                    <p class="card-text">${game.description}</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">${game.title}</button>
-                        </div>
-                        <small class="text-muted">${game.console}</small>
-                    </div>
-                </div>
-            </div>
-        </div>`
-    }
-    
-    // Display all games to the screen.
-    document.getElementById('mainContainer').innerHTML = content;
-}
-
-
-// Display info about a single game
+// Display info about specified game (e.g. mm1)
 function displayGame(game) {
     content = "";
 
@@ -76,4 +45,3 @@ function displayGame(game) {
     // Display all games to the screen.
     document.getElementById('mainContainer').innerHTML = content;
 }
-
