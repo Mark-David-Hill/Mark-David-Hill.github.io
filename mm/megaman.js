7aad52dfb710a075b615bfe9f642454950ebb50d
Akamarak <67317533+Mark-David-Hill@users.noreply.github.com>
2022-01-08 15:13:02 +0900

    Prototype Robot Masters section layout, begin work on displayGame() event.

--- mm/megaman.js
+++ mm/megaman.js
@@ -16,8 +16,6 @@ xhr.onload = function() {
     }
 } // end onload
 
-
-
 // Display all games
 function displayGames() {
     content = "";
@@ -34,7 +32,7 @@ function displayGames() {
                     <p class="card-text">${game.description}</p>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">${game.title}</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary gameBtn" id="${game.id}Btn">${game.title}</button>
                         </div>
                         <small class="text-muted">${game.console}</small>
                     </div>
@@ -45,35 +43,18 @@ function displayGames() {
     
     // Display all games to the screen.
     document.getElementById('mainContainer').innerHTML = content;
+
+    let gameBtns = document.getElementsByClassName('gameBtn');
+    for (let i = 0; i < gameBtns.length; i++) {
+        gameBtns[i].addEventListener("click", displayGame());
+    }
 }
 
 
 // Display info about a single game
-function displayGame(game) {
-    content = "";
-
-    // loop through each game
-    for (let i in mmData.games) {
-         //i is is the key for each game
-        let game = mmData.games[i];
-        // Generate HTML content for each game card
-        content += 
-            `<div class="col">
-                <div class="card shadow-sm"> 
-                    <img src="images/${game.id}.png" class="img-fluid" alt="${game} box-art"> <div class="card-body">
-                    <p class="card-text">${game.description}</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">${game.title}</button>
-                        </div>
-                        <small class="text-muted">${game.console}</small>
-                    </div>
-                </div>
-            </div>
-        </div>`
-    }
-    
-    // Display all games to the screen.
-    document.getElementById('mainContainer').innerHTML = content;
+function displayGame() {
+    console.log("Click Event");
 }
+
+// displayGame(mm1);
 
