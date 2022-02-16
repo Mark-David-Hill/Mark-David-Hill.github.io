6ad504d1ea4dd3beee7568063bd13c9d60be2529
Akamarak <67317533+Mark-David-Hill@users.noreply.github.com>
2022-02-16 17:16:00 +0900

    Make base HTML and JS files for to do list

---
+++ todo/todo.js
@@ -0,0 +1,56 @@
+// initialize
+(function () {
+    setClickEvents()
+  })();
+
+
+
+
+
+// 
+// Click Events
+// 
+
+// Setup click events
+
+function setClickEvents() {
+
+}
+
+
+
+
+// 
+// List Manager
+// 
+
+// Create new to do list
+// Create to do list template
+// Create to do list item
+// Remove to do list item
+
+
+
+
+
+
+// 
+// Display
+// 
+
+// Change styling of item if completed
+// Change order of list if completed?
+
+
+
+
+
+
+// 
+// Save/Load
+// 
+
+// Save template
+// Load template
+// Save list
+// Load list
