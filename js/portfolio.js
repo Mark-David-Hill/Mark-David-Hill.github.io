b8018259ab834a8c363b509ab529160a53ce0dde
Mark Hill <67317533+Mark-David-Hill@users.noreply.github.com>
2024-07-26 20:29:26 -0600

    adds e-commerce site and updates text of home and about pages

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,32 +3,31 @@ let content;
 
 // Get data from JSON file.
 let xhr = new XMLHttpRequest();
-xhr.open('GET', "./json/portfolioData.json", true);
-xhr.responseType = 'text';
+xhr.open("GET", "./json/portfolioData.json", true);
+xhr.responseType = "text";
 xhr.send();
 
-xhr.onload = function() {
-    if(xhr.status === 200) {
-        data = JSON.parse(xhr.responseText);
-        console.log(data);
-        displayProjects();
-    }
-} // end onload
+xhr.onload = function () {
+  if (xhr.status === 200) {
+    data = JSON.parse(xhr.responseText);
+    displayProjects();
+  }
+}; // end onload
 
 // Display project cards
 function displayProjects() {
-    content = "";
-    // Loop through project data to create html for cards
-    for (i = 0; i < data.length; i++) {
-        // Create html for a project's tag list items
-        let tagContent = "<ul>"
-        for (j = 0; j < data[i].tags.length; j++) {
-            tagContent += `<li>${data[i].tags[j]}</li>`
-        }
-        tagContent += `</ul>`;
-        
-        // Create HTML content for project card
-        content += `<div class="project-card">
+  content = "";
+  // Loop through project data to create html for cards
+  for (i = 0; i < data.length; i++) {
+    // Create html for a project's tag list items
+    let tagContent = "<ul>";
+    for (j = 0; j < data[i].tags.length; j++) {
+      tagContent += `<li>${data[i].tags[j]}</li>`;
+    }
+    tagContent += `</ul>`;
+
+    // Create HTML content for project card
+    content += `<div class="project-card">
         <img src="${data[i].image}" alt="${data[i].imageAlt}">
         <div>
             <p>${data[i].description}</p>
@@ -39,10 +38,7 @@ function displayProjects() {
             <a href="${data[i].gitHub}" target="blank">Code</a>
         </div>
         </div> <!-- End Project Card -->`;
-    }
-    // Write new HTML content to page.
-    document.getElementById('projects').innerHTML = content;
+  }
+  // Write new HTML content to page.
+  document.getElementById("projects").innerHTML = content;
 }
-
-
-
